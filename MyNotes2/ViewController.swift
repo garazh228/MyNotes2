0ac89299aae2d168b50323f4f31e7654a2c52f9f
garazh228 <adylbeksadykbekov.9.02@gmail.com>
2024-03-14 15:11:16 +0600

    Initial Commit

---
+++ MyNotes2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes2
+//
+//  Created by adyl CEO on 14/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
