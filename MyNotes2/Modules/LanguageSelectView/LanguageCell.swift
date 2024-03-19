e624d9f437ac764bbee4adddc994a781f53bab68
garazh228 <adylbeksadykbekov.9.02@gmail.com>
2024-03-19 17:40:18 +0600

    updated code

---
+++ MyNotes2/Modules/LanguageSelectView/LanguageCell.swift
@@ -0,0 +1,29 @@
+//
+//  LanguageCell.swift
+//  MyNotes2
+//
+//  Created by adyl CEO on 19/03/2024.
+//
+
+import UIKit
+
+class LanguageCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
